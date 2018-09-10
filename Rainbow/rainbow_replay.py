39a9a628eba076d1258baa504c967ed3749f19c1
Kim Choong Hyun <megastine@gmail.com>
2018-09-10 13:43:20 +0900

    Update rainbow_replay.py

--- Rainbow/rainbow_replay.py
+++ Rainbow/rainbow_replay.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import tensorflow as tf
 
 class SumTree:
     write = 0
@@ -7,8 +8,8 @@ class SumTree:
 
     def __init__(self, capacity):
         self.capacity = capacity
-        self.tree = numpy.zeros( 2*capacity - 1 )
-        self.data = numpy.zeros( capacity, dtype=object )
+        self.tree = np.zeros( 2*capacity - 1 )
+        self.data = np.zeros( capacity, dtype=object )
 
     def _propagate(self, idx, change):
         parent = (idx - 1) // 2
@@ -53,12 +54,12 @@ class SumTree:
 
     def get(self, s):
         idx = self._retrieve(0, s)
-        dataIdx = idx - self.capacity + 1
+        dataidx = idx - self.capacity + 1
 
-        return (idx, self.tree[idx], self.data[dataIdx])
+        return idx, self.tree[idx], self.data[dataidx]
 
-    def get_data(self, dataIdx):
-        return self.data[dataIdx]
+    def get_data(self, dataidx):
+        return self.data[dataidx]
 
 class PriorityExperienceReplay:
     '''
@@ -78,7 +79,6 @@ class PriorityExperienceReplay:
         self.e = 0.01
         self.a = 0.6
         self.t = 0
-        self.empty = tf.zeros_like()
 
     def _getPriority(self, error):
         return (error + self.e) ** self.a
@@ -88,20 +88,20 @@ class PriorityExperienceReplay:
             # 0.5 is the maximum error
             p = self._getPriority(0.5)
             self.tree.add(p, data=(self.t, o_s, a, r, n_s, i_t)) 
-            self.t = 0 if i_t else self.t += 1
+            # self.t = 0 if i_t else self.t += 1
 
 
-    def _get_transitions(self, dataIdx):
+    def _get_transitions(self, dataidx):
         transition = [None] * (self._window_size + self.num_step)
-        transition[self._window_size - 1] = self.tree.get_data(dataIdx) # 3
+        transition[self._window_size - 1] = self.tree.get_data(dataidx) # 3
         for i in range(self._window_size - 2, -1, -1): # 2, 1, 0
             if transition[i + 1][0] == 0:
                 transition[i] = tf.zeros_like(transition[self._window_size - 1])
             else:
-                transition[i] = self.tree.get_data(dataIdx - self._window_size + i + 1)
+                transition[i] = self.tree.get_data(dataidx - self._window_size + i + 1)
         for i in range(self._window_size, self._window_size + self.num_step): # 4, 5, 6
             if transition[i - 1][4] == False:
-                transition[i] = self.tree.get_data(dataIdx - self._window_size + i + 1)
+                transition[i] = self.tree.get_data(dataidx - self._window_size + i + 1)
             else:
                 transition[i] = tf.zeros_like(transition[self._window_size - 1])
         return transition
@@ -111,27 +111,27 @@ class PriorityExperienceReplay:
         while not valid:
             sample = random.uniform(i * segment, (i + 1) * segment)
             idx, prob, data = self.tree.get(sample)
-            dataIdx = idx - self._max_size + 1
+            dataidx = idx - self._max_size + 1
             if (self.tree.count - idx) % self._max_size > self.num_step and (idx - self.tree.count) % self._max_size >= self._window_size and prob != 0:
                 valid = True
 
-        transition = self._get_transition(dataIdx)
+        transition = self._get_transitions(dataidx)
         state = [trans[1] for trans in transition[:self._window_size]] #), tf.float32) # /255
         next_state = [trans[1] for trans in transition[self.num_step : self.num_step + self._window_size]] # /255
         action = [transition[self._window_size - 1][2]]
         reward = [sum(self.discount ** n * transition[self._window_size + n - 1][3] for n in range(self.num_step))] # 1dim
         is_terminal = [transition[self._window_size + self.num_step - 1][-1]]
-        return prob, dataIdx, idx, st`ate, action, reward, next_state, is_terminal
+        return prob, dataidx, idx, state, action, reward, next_state, is_terminal
 
     def sample(self, batch_size):
         p_total, count = self.tree.total_and_count()
         segment = p_total / batch_size
         batch = [self._get_sample_from_segment(segment, i) for i in range(batch_size)]
-        probs, dataIdxs, idxs, states, actions, rewards, next_states, is_terminals = zip(*batch)
+        probs, dataidxs, idxs, states, actions, rewards, next_states, is_terminals = zip(*batch)
         
         states = np.reshape(states, (-1, self._WIDTH, self._HEIGHT, self._window_size))
         next_states = np.reshape(next_states, (-1, self._WIDTH, self._HEIGHT, self._window_size))
-        #actions, returns, nonterminals = torch.cat(actions), torch.cat(returns), torch.stack(nonterminals)########################
+        # actions, returns, is_terminals =
         probs = probs / p_total
         weights = (count * probs) ** -self.priority_weight
         weights = weights / max(weights)
